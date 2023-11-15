c13fb7ed8a572e2c6e3ad880a7fd209f2e780396
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-11-15 19:06:34 +0000

    Bubble sort done

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				for (k = 0; k < size; k++)
+					printf("%d, ", array[k]);
+				printf("\n");
+			}
+		}
+	}
+}
