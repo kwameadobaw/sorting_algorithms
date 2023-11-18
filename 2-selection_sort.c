6cdbb18f5c93e95f6b3a351e3737f6037720d56b
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-11-18 13:35:48 -0800

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,33 +14,33 @@ void swap(int *a, int *b)
 }
 
 /**
- * selection_sort - Sort an array of integers in ascending order
+ * selection_sort - Sorts an array of integers in ascending order
  *                  using the selection sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, k;
+	size_t i, j;
 	size_t min_index;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
+		
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
 		}
 
-	if (min_index != i)
-	{
-		swap(&array[i], &array[min_index]);
-
-		for (k = 0; k < size; k++)
-			printf("%d, ", array[k]);
-
-		printf("\n");
-	}
+		if (min_index != i)
+		{
+			swap(&array[i], &array[min_index]);
+			print_array(array, size);
+		}
 	}
 }
