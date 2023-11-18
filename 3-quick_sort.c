7fb00e92ed246aefae20ad840b39d131cc101892
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-11-18 14:07:43 -0800

    again

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
  * quick_sort - Sorts an array of integers in ascending order
- * 		using the Quick sort algorithm
+ *		using the Quick sort algorithm
  * @array: An array of integers.
  * @size: The size of the array.
 */
