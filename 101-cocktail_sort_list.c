b8d7b74d97c361cd6804e57f7ee34bff61ee16b9
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-11-18 15:32:54 -0800

    trust

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,70 +7,68 @@
 */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *start = NULL, *end = NULL, *current = NULL;
-	int swapped;
+	int swapped = 1;
+	listint_t *current;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	do {
+	while (swapped)
+	{
 		swapped = 0;
 
-		for (current = *list; current->next != end; current = current->next)
+		/* Traverse from left to right */
+		for (current = *list; current->next != NULL; current = current->next)
 		{
 			if (current->n > current->next->n)
 			{
 				swap_nodes(list, &current, &(current->next));
+				print_list(*list);
 				swapped = 1;
 			}
 		}
+
 		if (!swapped)
 			break;
 
-		end = current;
-
 		swapped = 0;
 
-		for (current = current->prev;
-				current->prev != start; current = current->prev)
+		/* Traverse from right to left */
+		for (; current->prev != NULL; current = current->prev)
 		{
 			if (current->n < current->prev->n)
 			{
 				swap_nodes(list, &(current->prev), &current);
+				print_list(*list);
 				swapped = 1;
 			}
 		}
-
-		start = current;
-
-	} while (swapped);
+	}
 }
-
 /**
- * swap_nodes - Swaps two nodes in a doubly linked list
- * @list: A pointer to a pointer to the head of the list
- * @node1: A pointer to the first node
- * @node2: A pointer to the second node
+ * swap_nodes - Swaps two nodes in a doubly linked list.
+ * @list: A pointer to the head of the doubly linked list.
+ * @node1: A pointer to the first node to be swapped.
+ * @node2: A pointer to the second node to be swapped.
 */
 void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
 {
-	listint_t *temp1, *temp2;
+	listint_t *temp;
 
-	temp1 = (*node1)->prev;
-	temp2 = (*node2)->next;
+	temp = (*node1)->prev;
 
-	if (temp1 != NULL)
-		temp1->next = *node2;
-	else
-		*list = *node2;
+	if (temp != NULL)
+		temp->next = *node2;
 
-	if (temp2 != NULL)
-		temp2->prev = *node1;
-
-	(*node1)->next = temp2;
+	(*node2)->prev = temp;
 	(*node1)->prev = *node2;
-	(*node2)->next = *node1;
-	(*node2)->prev = temp1;
+	(*node1)->next = (*node2)->next;
 
-	print_list(*list);
+	if ((*node2)->next != NULL)
+		(*node2)->next->prev = *node1;
+
+	(*node2)->next = *node1;
+
+	if (temp == NULL)
+		*list = *node2;
 }
